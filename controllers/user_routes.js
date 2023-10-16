5df5b3d943b57b3bd1eaf4722266b3fec151dea0
JD Tadlock <jt7903@gmail.com>
2023-10-16 14:40:17 -0400

    register route complete

--- controllers/user_routes.js
+++ controllers/user_routes.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 
 // Import the User model
+const User = require('../models/User.js');
 
+// localhost:3333/auth/register
 // Post request route that retrieves the form data(email, password) and creates a new user in the database, using our User model
 // The route will respond with a data object with a property of message that says "User added successfully!"
+router.post('/register', (req, res) => {
+  const data = req.body;
+
+  User.create(data)
+    .then(newUser => {
+      res.json({
+        message: 'User added successfully',
+        user: newUser
+      });
+    });
+});
+
+module.exports = router;
 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // Import express
 const express = require('express');
+const db = require('./config/connection');
 
 // Import our view_routes
 const view_routes = require('./controllers/view_routes');
+const user_routes = require('./controllers/user_routes');
 
 // Create the port number and prepare for heroku with the process.env.PORT value
 const PORT = process.env.PORT || 3333;
@@ -13,8 +15,17 @@ const app = express();
 // Open the static channel for our browser assets - ie. express.static on the public folder
 app.use(express.static('./public'));
 
+// Allow json to be sent from the client
+app.use(express.json());
+
 // Load our view routes at the root level '/'
 app.use('/', view_routes);
+// /auth/register
+app.use('/auth', user_routes);
 
-// Start the server and log the port that it started on
-app.listen(PORT, () => console.log('Server is running on port', PORT));
+// Sync and create tables
+db.sync()
+  .then(() => {
+    // Start the server and log the port that it started on
+    app.listen(PORT, () => console.log('Server is running on port', PORT));
+  });
